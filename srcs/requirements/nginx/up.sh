5cfd22f350a9702e791e9de714d645d931d49c49
Eiji Nena <enena@va-g5.kzn.21-school.ru>
2021-07-25 11:32:45 +0300

    wtf nginx broken

--- srcs/requirements/nginx/up.sh
+++ srcs/requirements/nginx/up.sh
@@ -2,6 +2,6 @@
 if [ ! -d /etc/ssl/private/inception.key ]; then
     openssl req -x509 -nodes -days 1 -newkey rsa:2048 -subj "/C=RU/ST=Russia/L=Kazan/" \
         -keyout /etc/ssl/private/inception.key -out /etc/ssl/certs/inception.crt
-    mv ./nginx.conf /etc/nginx/
+    mv /nginx.conf /etc/nginx/
 fi
 exec nginx -g 'daemon off;'
