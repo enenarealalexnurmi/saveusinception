1251bd88fd936d0ffc4b1c01432bc2eba08441b7
Eiji Nena <enena@va-g5.kzn.21-school.ru>
2021-07-25 14:19:44 +0300

    please

--- srcs/requirements/nginx/up.sh
+++ srcs/requirements/nginx/up.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-if [ ! -d /etc/ssl/private/inception.key ]; then
-    openssl req -x509 -nodes -days 1 -newkey rsa:2048 -subj "/C=RU/ST=Russia/L=Kazan/" \
-        -keyout /etc/ssl/private/inception.key -out /etc/ssl/certs/inception.crt
-fi
-mv /nginx.conf /etc/nginx/
+openssl req -x509 -nodes -days 1 -newkey rsa:2048 -subj "/C=RU/ST=Russia/L=Kazan/"\
+-keyout /etc/ssl/private/inception.key -out /etc/ssl/certs/inception.crt
 exec nginx -g 'daemon off;'
