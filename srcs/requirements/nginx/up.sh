4139204550d72b9187859fdb50442fea08c019d0
Eiji Nena <enena@va-g5.kzn.21-school.ru>
2021-07-25 14:39:45 +0300

    dfk2

--- srcs/requirements/nginx/up.sh
+++ srcs/requirements/nginx/up.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-openssl req -x509 -nodes -days 1 -newkey rsa:2048 -subj "/C=RU/ST=Russia/L=Kazan/"\
--keyout /etc/ssl/private/inception.key -out /etc/ssl/certs/inception.crt
+openssl req -x509 -nodes -days 1 -newkey rsa:2048 -subj "/C=RU/ST=Russia/L=Kazan/" \
+    -keyout /etc/ssl/private/inception.key -out /etc/ssl/certs/inception.crt
 exec nginx -g 'daemon off;'
